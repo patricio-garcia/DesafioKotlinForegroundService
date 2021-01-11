791079a942e20691bdf8dc3e07a488237f93a42d
Patricio García A <pgarcia@serlitoral.cl>
2021-01-11 11:50:41 -0300

    Proyecto Kotlin que implementa Servicio en Primer Plano e informa de su ejecución a través de una notificación

---
+++ app/src/main/java/cl/serlitoral/desafiokotlinforegroundservice/ForegroundService.kt
@@ -0,0 +1,69 @@
+package cl.serlitoral.desafiokotlinforegroundservice
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.os.IBinder
+import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
+
+class ForegroundService : Service() {
+
+    private val channelId = "Kotlin - Servicio en Primer Plano"
+
+    companion object {
+        fun startService(context: Context, message: String) {
+            val startIntent = Intent(context, ForegroundService::class.java)
+            startIntent.putExtra("inputExtra", message)
+            ContextCompat.startForegroundService(context, startIntent)
+        }
+
+        fun stopService(context: Context) {
+            val stopIntent = Intent(context, ForegroundService::class.java)
+            context.stopService(stopIntent)
+        }
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        //Se hace una tarea en un hilo
+        val input = intent?.getStringExtra("inputExtra")
+        createNotificationChannel()
+        val notificationIntent = Intent(this, MainActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            notificationIntent, 0
+        )
+
+        //Se define la notificación
+        val notification = NotificationCompat.Builder(this, channelId)
+            .setContentTitle("Ejemplo de Servicio en Primer Plano")
+            .setContentText(input)
+            .setSmallIcon(R.drawable.ic_notification)
+            .setContentIntent(pendingIntent)
+            .build()
+        startForeground(1, notification)
+        return START_NOT_STICKY
+    }
+
+    override fun onBind(intent: Intent): IBinder? {
+        return null
+    }
+
+    //Se crea la notificación
+    private fun createNotificationChannel() {
+        //Se valida la version del SDK para uso del Canal de Notificación (Android O o superior)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val serviceChannel = NotificationChannel(channelId,
+                "Canal de Servicio en Primer Plano",
+                NotificationManager.IMPORTANCE_DEFAULT)
+
+            val manager = getSystemService(NotificationManager::class.java)
+            manager!!.createNotificationChannel(serviceChannel)
+        }
+    }
+}
---
+++ app/src/main/java/cl/serlitoral/desafiokotlinforegroundservice/MainActivity.kt
@@ -0,0 +1,24 @@
+package cl.serlitoral.desafiokotlinforegroundservice
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import cl.serlitoral.desafiokotlinforegroundservice.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonStart.setOnClickListener(View.OnClickListener {
+            ForegroundService.startService(this, "Servicio en Primer Plano Ejecutándose...")
+        })
+
+        binding.buttonStop.setOnClickListener(View.OnClickListener {
+            ForegroundService.stopService(this)
+        })
+    }
+}
